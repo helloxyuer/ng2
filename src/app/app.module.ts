d9eddb9a6d9290992af609d321e002630b609359
bychen <874742015@qq.cpm>
2017-09-28 14:08:35 +0800

    周4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,12 +9,12 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
